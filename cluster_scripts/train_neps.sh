0a274d2c7f096641682bff262501af99aa2bc8cf
Diane Wagner <wagnerd@informatik.uni-freiburg.de>
2025-02-13 21:27:43 +0100

    Update submission script

--- cluster_scripts/train_neps.sh
+++ cluster_scripts/train_neps.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-##SBATCH -p alldlc_gpu-rtx2080
-#SBATCH -p testdlc_gpu-rtx2080
+#SBATCH -p alldlc_gpu-rtx2080
+##SBATCH -p testdlc_gpu-rtx2080
 ##SBATCH -q dlc-wagnerd
 #SBATCH --gres=gpu:1
 #SBATCH -J medquicktune
 ##SBATCH -t 23:59:59
-#SBATCH --array 0-19%5
+##SBATCH --array 0-19%5
 
 source activate medquicktune
 
